19ce16ffab7a4b80879b8aee381c8cc7a74ef264
dayviddouglas <dayvidacademico@gmail.com>
2024-09-03 17:29:47 -0300

    Requisitando a chave da api

--- index.js
+++ index.js
@@ -27,21 +27,58 @@ function addMessage(sender, text) {
         "qual é a tradução de 'comprar'?": "A tradução de 'comprar' é 'buy'."
     };
    
-    async function get_api_key(){
-        await fetch('config.json')
-        .then(response => response.json())
-        .then(config => {
-           return config.API_KEY; 
-        });
+   async function get_api_key() {
+        try {
+            const response = await fetch('config.json')
+            const response2 = await response.json();
+            return response2.API_KEY
+        } catch (error) {
+            throw new Error("Erro na busca pela chave da API");
+        }
+   }
+
+
+
+
+    async function request_api() {
+             get_api_key().then((response)=>{
+                chave_api= response;
+                try {
+                    const call_chatgpt = 
+                } catch (error) {
+                    
+                }
+
+
+            });
+         
+
     }
 
-    async function request_api(user_message) {
-       let chave_api= get_api_key();
-       console.log(chave_api);
 
-    }
+    
 
-    request_api();
+// async function getApiKey() {
+//     try {
+//         const response = await fetch('config.json');
+//         const config = await response.json();
+//         return config.API_KEY; // Retorne o valor desejado
+//     } catch (error) {
+//         throw new Error('Erro ao buscar a chave da API'); // Lançar um erro para capturá-lo no .catch
+//     }
+// }
+
+// getApiKey()
+//     .then((response) => {
+//         console.log(response); // Mostra a API_KEY no console
+//     })
+//     .catch((err) => {
+//         console.error(err); // Mostra o erro no console
+//     });
+
+    
+    
+
 
 
 
